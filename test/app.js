bf0fb973d10335484c7bffbe61756711f8cbe519
Yuri2 <824831811@qq.com>
2017-09-26 17:36:50 +0800

    目录结构调整

--- test/app.js
+++ test/app.js
@@ -4,13 +4,14 @@ const yuri2 = require('../yuri2');
 
 let app = yuri2.yuri2Server.createServer({
     name:'app1',
-    spirit:true,
     debug:true,
 });
-let router = yuri2.yuri2Server.createRouter().load(__dirname+'/route_table');
-
-// app.use(yuri2.yuri2Server.getMiddleware('spirit')());
-app.use(yuri2.yuri2Server.getMiddleware('cookie-session')());
+let router = yuri2.yuri2Server.createRouter();
+router.load(__dirname+'/app/routes');
+app.use(yuri2.yuri2Server.getMiddleware('spirit')());
+app.use(yuri2.yuri2Server.getMiddleware('not-found')({msg:['天啊，那页失踪了','返回首页'], home:'/'}));
+app.use(yuri2.yuri2Server.getMiddleware('cookie-session')(app));
+app.use(yuri2.yuri2Server.getMiddleware('pug-support')());
 app.use(yuri2.yuri2Server.getMiddleware('state-control')(app));
 app.use(router.parse());
 app.use(yuri2.yuri2Server.getMiddleware('static')('./public'));
---
+++ test/app1/routes/route_table1.js
@@ -0,0 +1,8 @@
+//动态加载路由表
+const yuri2=require('../../../yuri2');
+module.exports=function (router) {
+    router.get('/test1',async function (ctx,next) {
+        ctx.dump(1);
+        await next();
+    });
+};
